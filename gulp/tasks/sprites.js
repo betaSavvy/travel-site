9b2f5fc27aff4d57fa228f8726b1518e5caa044a
Delonn Koh <delonnkoh.dev@gmail.com>
2016-10-03 21:45:00 +0800

    Create gulp task to copy sprite.css to modules and rename to _sprite.css. Clean up code to create sprite in images/icons/sprites folder itself instead of temp folder

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp'),
-    svgSprite = require('gulp-svg-sprite');
+    svgSprite = require('gulp-svg-sprite'),
+		rename = require('gulp-rename');
 
 var config = {
 		mode: {
 			css: {
+				sprite: 'sprite.svg',
 				render: {
 					css: {
 						template: './gulp/templates/sprite.css'
@@ -16,5 +18,13 @@ var config = {
 gulp.task('createSprite', function() {
 	return gulp.src('./app/assets/images/icons/**/*.svg')
 	.pipe(svgSprite(config))
-	.pipe(gulp.dest('./app/temp/sprite/'));
+	.pipe(gulp.dest('./app/assets/images/icons/sprites/'));
 });
+
+gulp.task('copySpriteCSS', ['createSprite'], function() {
+	return gulp.src('./app/assets/images/icons/sprites/css/*.css')
+	.pipe(rename('_sprite.css'))
+	.pipe(gulp.dest('./app/assets/styles/modules'));
+});
+
+gulp.task('icons', ['copySpriteCSS']);
